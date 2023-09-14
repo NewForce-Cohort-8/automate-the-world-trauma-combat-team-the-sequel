d1ef4d5fd24430b42591541d939c6d43b90eb60d
Justin Ireland <justinireland90@yahoo.com>
2023-09-14 11:18:57 -0400

    Additions to Country Cards

--- scripts-destData/countrylist.js
+++ scripts-destData/countrylist.js
@@ -14,10 +14,8 @@ export const countryList = () => {
 
         
         htmlString += `<section class="dest-card">
-            <div><img class="dest__image" src="${country.image}" /></div>
-            <div class="dest__title"> <span>title:</span> ${ country.Title}</div>
-            <div class="dest__landmark"> <span>landmark:</span> ${country.Landmark}</div>
-            <div><a href= "${country.Link}"> Learn More </a></div>
+            <div> <a href="${country.Link}"><img class="dest__image" src="${country.image}" /></a></div>
+            <div class="dest-name">  ${ country.Title}</div>
 
           </section>
          `
